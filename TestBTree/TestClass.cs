90acde97a04653b764c40ad9b09533db912d66ad
AZanovskaya <azanovskaya@softrust.ru>
2019-12-15 17:32:36 +0300

    init

---
+++ TestBTree/TestClass.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestBTree
+{
+    [TestFixture]
+    class TestClass
+    {
+        [Test]
+        public void TestSuccess()
+        {
+            Assert.AreEqual(1, 1);
+        }
+        [Test]
+        public void TestFail()
+        {
+            Assert.AreEqual(1, 4);
+        }
+    }
+}
