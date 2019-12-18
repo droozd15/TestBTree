941095b5a6ff25f7ff6c00a33f69265a72b09efb
AZanovskaya <azanovskaya@softrust.ru>
2019-12-18 16:37:10 +0300

    Search, Insert BTree

--- TestBTree/Node.cs
+++ TestBTree/Node.cs
@@ -12,8 +12,8 @@ namespace TestBTree
         public List<Node<T>> Children { get; set; }
         public List<Entry<T>> Entries { get; set; }
 
+        public bool IsLeaf => this.Children.Count == 0;
         public bool HasReachedMaxEntries => this.Entries.Count == (2 * this._degree) - 1;
-
         public Node(int degree)
         {
             this._degree = degree;
